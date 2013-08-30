61951f20c0793c242155259fcdc3161acf1376f3
U-OEMSURPLUS\chris.mason <chris.mason@M4700-8QF7JX1.oemsurplus.local>
2013-08-30 12:44:58 -0700

    Fixed to an initial runnable state

--- parser.py
+++ parser.py
@@ -15,85 +15,94 @@ import sys
 import binascii
 
 def extract_msg_from_line(line):
-	""" Returns the transmitted char logged by a line, and whether it was
-	a Tx or an Rx. Returned char is in bytes format
 
-	>>> extract_msg_from_line("78 08/21/13 17:10:37.007.295 2c , 1 0  0 0 0 0")
-	(2c, tx)
-	>>> extract_msg_from_line("83 08/21/13 17:10:37.071.151 0 0 0d < CR> 0 0 0 0")
-	(0d, rx)
-	"""
+    """ Returns the transmitted char logged by a line, and whether it was
+    a Tx or an Rx. Returned char is in bytes format
 
-	myline = line.split()
-	if len(myline[3]) == 2:
-		return (binascii.unhexlify(myline[3]), "tx")
-	elif(len(myline[5] == 2)):
-		return (binascii.unhexlify(myline[5]), "rx")
-	else:
-		return (b'', "null")
+    >>> extract_msg_from_line("78 08/21/13 17:10:37.007.295 2c , 1 0  0 0 0 0")
+    (2c, tx)
+    >>> extract_msg_from_line("83 08/21/13 17:10:37.071.151 0 0 0d < CR> 0 0 0 0")
+    (0d, rx)
+    """
+    
+    myline = line.split()
+    if len(myline[3]) == 2:
+        return (binascii.unhexlify(myline[3].encode()), "tx")
+    elif (len(myline[5]) == 2):
+        return (binascii.unhexlify(myline[5].encode()), "rx")
+    else:
+        return (b'', "null")
 
 
 def parse_file(txtfile):
-	"""Parses an EZ-View output textfile into a text file containing a 
-	chronological list of command / response pairs between master and slave.  
-	File also contains a set of all commands / responses.  The output file
-	is named after the base of the input with _parsed.txt appended. """
+    """Parses an EZ-View output textfile into a text file containing a 
+    chronological list of command / response pairs between master and slave.  
+    File also contains a set of all commands / responses.  The output file
+    is named after the base of the input with _parsed.txt appended. """
 
-	#open file
-	try:
-		inhandle = open(txtfile, 'r')
-	except: IOError as err:
-		print("Infile I/O error: {0}".format(err))
-		return
+    #open file
+    try:
+        print("Opening {0} for reading...".format(txtfile))
+        inhandle = open(txtfile, 'r')
+    except IOError as err:
+        print("Infile I/O error: {0}".format(err))
+        return
 
-	#open output file or die
-	try:
-		outfile = str(os.path.splitext(txtfile)[0] + "_parsed.txt")
-		outhandle = open(outfile, 'x')
-	except IOError as err:
-		print("Outfile I/O error: {0}".format(err))
-		return
+    #open output file or die
+    try:
+        outfile = str(os.path.splitext(txtfile)[0] + "_parsed.txt")
+        print("Opening {0} for writing..".format(outfile))
+        outhandle = open(outfile, 'w')
+    except IOError as err:
+        print("Outfile I/O error: {0}".format(err))
+        return
 
-	#throw away first line since it is just header garbage
-	curline = inhandle.readline()
+    #throw away first line since it is just header garbage
+    curline = inhandle.readline()
 
-	curdirection = "null"
-	allcmds = set()
-	curmsg = bytearray()
+    curdirection = "null"
+    alltxcmds = set()
+    allrxcmds = set()
+    curmsg = bytearray()
 
-	#while not end of file:
-	for line in inhandle:
-		(txbyte, direction) = extract_msg_from_line(inhandle.readline())
+    #while not end of file:
+    for line in inhandle:
+        (txbyte, direction) = extract_msg_from_line(line)
 
-		#if message is done, reset message vars
-		if direction != curdirection:
-			if curmsg != b'':
-				outhandle.write(curmsg.decode())
-				outhandle.write('\n')
-				allcmds.add(curmsg.decode())
-				curmsg = b''
-			curdirection = direction
-			#see if tx or rx, print to output accordingly
-			if ("null" == direction):
-				#do nothing
-				pass
+        #direction change
+        if direction != curdirection:
+            #we just parsed a full command, place in correct set and reset var
+            if curmsg != b'':
+                outhandle.write(curmsg.decode())
+                outhandle.write('\n')            
+                if ("rx" == curdirection):
+                    allrxcmds.add(curmsg.decode().strip())
+                elif ("tx" == curdirection):
+                    alltxcmds.add(curmsg.decode().strip())
+                curmsg = b''
+                                
+            #see if new dir is tx or rx, print to output accordingly
+            if ("null" == direction):
+                #do nothing
+                pass
+            elif ("rx" == direction):
+                outhandle.write("Robot responded: ")
+            elif ("tx" == direction):
+                alltxcmds.add(curmsg.decode().strip())
+                outhandle.write("Master transmitted: ")
+                
+            curdirection = direction
 
-			elif ("rx" == direction):
-				outhandle.write("Slave transmitted: ")
-				while direction == "rx":
-					curmsg += txbyte
+        curmsg += txbyte
 
-			elif ("tx" == direction):
-				outhandle.write("Master transmitted: ")
-
-		curmsg += txbyte
-
-	outhandle.write('\n\n')
-	outhandle.write("List of all commands seen:\n")
-	outhandle.write(str(allcmds))
-	outhandle.close()
-	inhandle.close()
+    outhandle.write("\n\nList of all master commands seen:\n")
+    outhandle.write(str(alltxcmds))
+    outhandle.write("\n\nList of all slave responses seen:\n")
+    outhandle.write(str(allrxcmds))    
+    outhandle.close()
+    inhandle.close()
+    print("Done!")
 
 if __name__ == "__main__":
-	parse_file(sys.argv[1])
+    parse_file(sys.argv[1])
 
